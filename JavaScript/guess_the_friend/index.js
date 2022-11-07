56ccb22e4160e428297de4ca8ebc98f64926faf2
risto_nestorovic <risto.nestorovic@inform-technologies.com>
2022-11-07 19:16:53 +0100

    switch statement

---
+++ JavaScript/guess_the_friend/index.js
@@ -0,0 +1,19 @@
+const friend = prompt("Please enter the name.")
+
+
+switch (friend) {
+  case "Nemanja":
+    console.log("Nemanja is your friend !");
+    break;
+  case "Milos":
+    console.log("Milos is your friend !");
+    break;
+  case "Nikola":
+    console.log("Nikola is your friend !");
+    break;
+  case "Marko":
+    console.log("Marko is your friend !");
+    break;
+  default :
+    console.log(friend + " isn't in list of your friend's ! ");
+}
