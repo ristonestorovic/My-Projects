e6bd3152ebd432b2694f7fc389cace912ddb335d
risto_nestorovic <risto.nestorovic@inform-technologies.com>
2022-12-26 17:05:35 +0100

    querySelector implemented.

--- JavaScript/guess_the_number/starter/script.js
+++ JavaScript/guess_the_number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+
+document.querySelector('.message').textContent = " Risto ❤"  
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
