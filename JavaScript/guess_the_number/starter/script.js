ed111aac2909adfef9363157050fa66fb44c2d6a
risto_nestorovic <risto.nestorovic@inform-technologies.com>
2022-12-27 13:51:21 +0100

    Guess the number project finished.

--- JavaScript/guess_the_number/starter/script.js
+++ JavaScript/guess_the_number/starter/script.js
@@ -1,10 +1,54 @@
 'use strict';
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
-document.querySelector('.message').textContent = " Risto ❤"  
+const displayMessage = message => document.querySelector('.message').textContent = message;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  displayMessage(" Start guessing...");
+  document.querySelector('body').style.backgroundColor="#222";
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = '';
+})
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function(){
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+
+  if( !guess) {
+    displayMessage(" No Number ⛔");
+
+  //When player wins
+
+  } else if ( guess === secretNumber) {
+    displayMessage("Correct Number 🎉");
+    document.querySelector('body').style.backgroundColor="#60b347";
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if( score > highscore){
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+  //When guess is wrong
+
+  } else if ( guess !== secretNumber){
+    if( score > 1) {
+      displayMessage(guess > secretNumber ? "Too high !!!" : "Too low !!!");
+
+      score -- ;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage("You lost the game 😪");
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+})
